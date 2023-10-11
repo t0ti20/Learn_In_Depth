2b8eed00505ecb16ffc01d87cc734f3bf5f2352d
Khaled El-Sayed <t0ti20@icloud.com>
2023-10-11 17:59:55 +0300

    Update My RTOS

--- 14 - ARM/SVC-PendSV/COTS/Application/Application.c
+++ 14 - ARM/SVC-PendSV/COTS/Application/Application.c
@@ -6,7 +6,7 @@
 #define Switch_MSP						__asm volatile	("MRS R0,CONTROL\n\tBFC R0,#1,#1\n\tMSR CONTROL,R0")
 #define Switch_Privileged				__asm volatile ("MRS R0,CONTROL\n\tBFC R0,#0,#1\n\tMSR CONTROL,R0")
 #define Switch_Unprivileged				__asm volatile ("MRS R0,CONTROL\n\tORR R0,R0,#1\n\tMSR CONTROL,R0")
-#define Supervisor_Call					__asm volatile	("SVC #3")
+#define Supervisor_Call(Number)			__asm volatile	("SVC #"#Number)
 /*--------------------------------*/
 typedef enum Services_t
 {
@@ -43,38 +43,31 @@ void SVC_Number (u32*Stack_Pointer)
 }
 __attribute ((naked)) void SVC_Handler(void)
 {
-	__asm("TST LR,#4\n\tITE EQ\n\tMRSEQ r0,MSP\n\tMRSNE r0,PSP\n\tB SVC_Number");
+	__asm("TST LR,#4\n\t" "ITE EQ\n\t" "MRSEQ r0,MSP\n\t" "MRSNE r0,PSP\n\t" "B SVC_Number");
 }
 /*--------------------------------*/
 u32 OS_Service(u32 Argument_1,u32 Argument_2,Services_t Service_ID)
 {
 	u32 Result=0;
+	;
 	switch (Service_ID)
 	{
-		case Add:__asm("SVC #1");break;
-		case Multiply:__asm("SVC #2");break;
-		case Subtraction:__asm("SVC #3");break;
+		case Add:Supervisor_Call(1);break;
+		case Multiply:Supervisor_Call(2);break;
+		case Subtraction:Supervisor_Call(3);break;
 	}
 	return Result;
-}
-/*--------------------------------*/
-void isr(void)
-{
-	Flag^=1;
 }
 /*--------------------------------*/
 int main(void) 
  {
 	u32 Result=100;
-	EXTI_Config_t EXTI_1={GPIO_A,Pin_0,Faling_Rising,Enable,Disable,isr};
 	GPIO_Pin_Config_t Pin={Pin_1,Output_Push_Pull,Output_10};
 /*--------------------------------*/
-	EXTI_Initilization(EXTI_1);
 	MCAL_GPIO_Initialize(GPIO_A,Pin);
 /*--------------------------------*/
 	while (1)
 	{
 		Result=OS_Service(10,20,Subtraction);
-		if(Flag)MCAL_GPIO_Toggle_Pin(GPIO_A,Pin_1);
 	}
 }
